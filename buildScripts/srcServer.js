2d36ab359e6a2e1b099e3288684f6b7a8d1203c8
Surajkumar1992 <suraj.subham@gmail.com>
2021-06-15 13:00:58 +0530

    recent commits

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -17,6 +17,14 @@ app.get('/',function(req, res){
     res.sendFile(path.join(__dirname + '/../src/index.html'));
     });
 
+app.get('/user', function(req, res){
+    res.json([
+        {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob_smith@gmail.com"},
+        {"id": 2, "firstName": "Kyle", "lastName": "Reese", "email": "klreese@gmail.com"},
+        {"id": 3, "firstName": "Chan", "lastName": "yung", "email": "chany@gmail.com"}
+    ])
+});
+
 /*eslint-disable no-console*/
 
 app.listen(port, function(err){
